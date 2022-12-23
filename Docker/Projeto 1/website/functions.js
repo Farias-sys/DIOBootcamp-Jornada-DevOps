680cdf3ec739b08464ec819c89e9c1e032ce131f
Farias-sys <cafsalgadojr@gmail.com>
2022-12-22 23:20:00 -0300

    Docker - Project 1

---
+++ Docker/Projeto 1/website/functions.js
@@ -0,0 +1,11 @@
+function abrirGithub(){
+    window.open("https://github.com/Farias-sys")
+}
+
+function abrirInstagram(){
+    window.open("https://www.instagram.com/carlosafariajr/")
+}
+
+function abrirWhatsapp(){
+    window.open("https://www.wa.me/5521971205402")
+}
